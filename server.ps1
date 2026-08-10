432fc6d0ae7d1c5fd963aac87cc4246848fea5f2
Anthuan Vasquez <me@anthuanvasquez.net>
2026-08-10 17:13:57 -0400

    feat(server): add -Name param and document ISO dependency

--- server.ps1
+++ server.ps1
@@ -1,14 +1,24 @@
-VBoxManage createvm --name "ubuntu-template" --ostype "Ubuntu_64" --register
+# Usage: .\server.ps1 [-Name "ubuntu-template"]
+param(
+    [string]$Name = "ubuntu-template"
+)
 
-VBoxManage modifyvm "ubuntu-template" \
-  --memory 2048 \       # 2GB RAM
-  --cpus 2 \            # 2 CPU cores
-  --nic1 bridged --bridgeadapter1 "[REPLACE-WITH-YOUR-NETWORK-INTERFACE]" \
-  --graphicscontroller vmsvga \
+# Dependency: download the Ubuntu Server ISO and place it in this directory.
+# https://ubuntu.com/download/server
+# Expected filename: ubuntu-24.04.4-live-server-amd64.iso
+$ISO = "ubuntu-24.04.4-live-server-amd64.iso"
+
+VBoxManage createvm --name "$Name" --ostype "Ubuntu_64" --register
+
+VBoxManage modifyvm "$Name" `
+  --memory 2048 `       # 2GB RAM
+  --cpus 2 `            # 2 CPU cores
+  --nic1 bridged --bridgeadapter1 "[REPLACE-WITH-YOUR-NETWORK-INTERFACE]" `
+  --graphicscontroller vmsvga `
   --boot1 dvd --boot2 disk
 
-VBoxManage createmedium disk --filename "ubuntu-template.vdi" --size 20000 # 20GB disk size
+VBoxManage createmedium disk --filename "$Name.vdi" --size 20000 # 20GB disk size
 
-VBoxManage storagectl "ubuntu-template" --name "SATA" --add sata --controller IntelAHCI
-VBoxManage storageattach "ubuntu-template" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "ubuntu-template.vdi"
-VBoxManage storageattach "ubuntu-template" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium "ubuntu-24.04.4-live-server-amd64.iso"
+VBoxManage storagectl "$Name" --name "SATA" --add sata --controller IntelAHCI
+VBoxManage storageattach "$Name" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$Name.vdi"
+VBoxManage storageattach "$Name" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium "$ISO"
