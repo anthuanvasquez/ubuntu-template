ddc27cebb87182287ca863107b5205ec82ad388f
Anthuan Vasquez <me@anthuanvasquez.net>
2026-08-11 18:42:55 -0400

    style(server): reformat VBoxManage commands for readability

--- server.ps1
+++ server.ps1
@@ -24,25 +24,44 @@ if (-not (Test-Path $ISO)) {
 }
 
 # 1. Create and register the VM
-VBoxManage createvm --name "$Name" --ostype "Ubuntu_64" --register
+VBoxManage createvm --name "$Name" `
+    --ostype "Ubuntu_64" `
+    --register
 
 # 2. Configure hardware (2GB RAM, 2 CPUs, bridged network, boot from DVD first)
 VBoxManage modifyvm "$Name" `
-  --memory $Memory `
-  --cpus $Cpus `
-  --nic1 bridged --bridgeadapter1 "$NetworkAdapter" `
-  --graphicscontroller vmsvga `
-  --boot1 dvd `
-  --boot2 disk `
-  --ioapic on `
-  --firmware efi `
-  --nestedpaging on `
-  --vtxvpid on `
-  --paravirtprovider kvm
+    --memory $Memory `
+    --cpus $Cpus `
+    --nic1 bridged --bridgeadapter1 "$NetworkAdapter" `
+    --graphicscontroller vmsvga `
+    --boot1 dvd `
+    --boot2 disk `
+    --ioapic on `
+    --firmware efi `
+    --nestedpaging on `
+    --vtxvpid on `
+    --paravirtprovider kvm
 
 # 3. Create virtual disk and mount storage
-VBoxManage createmedium disk --filename "$UserVboxPath\$Name.vdi" --size ($DiskGB * 1024) # disk size in MB
+VBoxManage createmedium disk `
+    --filename "$UserVboxPath\$Name.vdi" `
+    --size ($DiskGB * 1024) # disk size in MB
 
-VBoxManage storagectl "$Name" --name "SATA" --add sata --controller IntelAHCI
-VBoxManage storageattach "$Name" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$UserVboxPath\$Name.vdi"
-VBoxManage storageattach "$Name" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium "$ISO" # mount ISO
+VBoxManage storagectl "$Name" `
+    --name "SATA" `
+    --add sata `
+    --controller IntelAHCI
+
+VBoxManage storageattach "$Name" `
+    --storagectl "SATA" `
+    --port 0 `
+    --device 0 `
+    --type hdd `
+    --medium "$UserVboxPath\$Name.vdi"
+
+VBoxManage storageattach "$Name" `
+    --storagectl "SATA" `
+    --port 1 `
+    --device 0 `
+    --type dvddrive `
+    --medium "$ISO" # mount ISO
