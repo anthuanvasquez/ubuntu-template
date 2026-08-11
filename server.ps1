ad678861ba0d75983ecb24a4b5df405e627669de
Anthuan Vasquez <me@anthuanvasquez.net>
2026-08-10 23:51:55 -0400

    fix(server): use full VDI path in storageattach to match createmedium

--- server.ps1
+++ server.ps1
@@ -7,6 +7,8 @@ param(
 # https://ubuntu.com/download/server
 # Expected filename: ubuntu-24.04.4-live-server-amd64.iso
 $ISO = "ubuntu-24.04.4-live-server-amd64.iso"
+$NetworkAdapter = "[REPLACE-WITH-YOUR-NETWORK-INTERFACE]"
+$UserVboxPath = "$env:USERPROFILE\VirtualBox VMs\$Name"
 
 # 1. Create and register the VM
 VBoxManage createvm --name "$Name" --ostype "Ubuntu_64" --register
@@ -15,13 +17,13 @@ VBoxManage createvm --name "$Name" --ostype "Ubuntu_64" --register
 VBoxManage modifyvm "$Name" `
   --memory 2048 `
   --cpus 2 `
-  --nic1 bridged --bridgeadapter1 "[REPLACE-WITH-YOUR-NETWORK-INTERFACE]" `
+  --nic1 bridged --bridgeadapter1 "$NetworkAdapter" `
   --graphicscontroller vmsvga `
   --boot1 dvd --boot2 disk
 
 # 3. Create virtual disk and mount storage
-VBoxManage createmedium disk --filename "$Name.vdi" --size 20000 # 20GB disk size
+VBoxManage createmedium disk --filename "$UserVboxPath\$Name.vdi" --size 20000 # 20GB disk size
 
 VBoxManage storagectl "$Name" --name "SATA" --add sata --controller IntelAHCI
-VBoxManage storageattach "$Name" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$Name.vdi"
+VBoxManage storageattach "$Name" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$UserVboxPath\$Name.vdi"
 VBoxManage storageattach "$Name" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium "$ISO" # mount ISO
